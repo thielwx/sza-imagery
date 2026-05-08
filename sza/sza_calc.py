f4397cb1933f7dfcc0f9c3c07cec5c35c327a4f5
thielwx <kevin.thiel@ou.edu>
2026-05-08 19:05:26 +0000

    Attempting to fix holes in bright imagery in AWIPS

--- sza/sza_calc.py
+++ sza/sza_calc.py
@@ -161,8 +161,8 @@ def sza_calculator_v2_exact(dset, sza_threshold=88.85, sza_nu=0.968):
     #Calculating the cosine of solar zenith angle
     cos_zen_grid = pyob_cos_zen(time, lons, lats)
 
-    #Setting all night values to 0 so its multplying by the max (determined by nu) at sunset
-    cos_zen_grid[cos_zen_grid<0] = 0
+    #Setting all night values to a small number so its multplying by the max (determined by nu) at sunset
+    cos_zen_grid[cos_zen_grid<=0] = 1e-4
     
     #Making a frequency adjustment to the grid to reduce the impact of Mie Scattering in moist environments
     zen_grid_rad = np.arccos(cos_zen_grid)
@@ -171,7 +171,8 @@ def sza_calculator_v2_exact(dset, sza_threshold=88.85, sza_nu=0.968):
     
     #Applying the solar zenith angle adjustment to the ABI reflectance factor data
     cmi = dset['CMI'][:] #Reflectance factor
-    sza_cmi = cmi / cos_zen_grid
+    sza_cmi = np.clip(cmi / cos_zen_grid, a_max=129.999) #Clipping so we aren't going over the max value in AWIPS
+    sza_cmi = np.round(sza_cmi, decimals=5) #Rounding to avoid bit depth issues between float64 and int32 in the CMI netCDFs
 
     return sza_cmi
 
